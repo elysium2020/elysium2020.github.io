b3304d2b50fdf5c252d22ea3ec1dd8fba51053a6
elysium2020 <elysium2020@users.noreply.github.com>
2025-05-16 11:00:53 +0800

    feat: add stylelint check

---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+//@ts-check
+
+export default {
+  extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
+  plugin: 'stylelint-plugin-tailwindcss',
+  rules: {
+    'at-rule-no-deprecated': [
+      true,
+      {
+        ignoreAtRules: ['apply'],
+      },
+    ],
+  },
+};
